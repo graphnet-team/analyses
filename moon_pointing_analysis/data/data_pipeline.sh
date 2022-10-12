56ab3c7462642a40ebf478081df72edd123aa103
Morten Holm <qgf305@hep02.cluster>
2022-10-12 13:51:04 +0200

    using the new conversion script

--- moon_pointing_analysis/data/data_pipeline.sh
+++ moon_pointing_analysis/data/data_pipeline.sh
@@ -1,5 +1,20 @@
-db=/groups/icecube/qgf305/storage/I3_files/Sebastian_MoonL4/
-gcd=None
-pulse="SRTInIcePulses"
 
-python convert_i3_to_sqlite.py --db ${db} --gcd ${gcd} --pulse ${pulse}
+# MC data
+#db=/mnt/lfs6/sim/IceCube/2012/filtered/level2/neutrino-generator/11069/09000-09999/
+#gcd=/mnt/lfs6/sim/IceCube/2012/filtered/level2/neutrino-generator/11069/08000-08999/GeoCalibDetectorStatus_2012.56063_V1.i3.gz
+
+# MC data; local
+db=/scratch/mholm/database/I3files/
+gcd=/scratch/mholm/database/I3files/GeoCalibDetectorStatus_2012.56063_V1.i3.gz
+
+pulse="InIceDSTPulses"
+
+outdir=/scratch/mholm/database/
+report_location=/data/user/mholm/data/nohup_reports/MCL21.out
+
+nohup python /data/user/mholm/graphnet/studies/Moon_Pointing_Analysis/data/convert_i3_files.py \
+--db ${db} \
+--gcd ${gcd} \
+--pulse ${pulse} \
+--outdir ${outdir} \
+> ${report_location}
