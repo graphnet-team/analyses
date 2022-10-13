42122e921a9c5c11c1e66a347a4644a429dd8ae9
Morten Holm <qgf305@hep02.cluster>
2022-10-13 08:58:54 +0200

    user independent shell

--- moon_pointing_analysis/data/data_pipeline.sh
+++ moon_pointing_analysis/data/data_pipeline.sh
@@ -1,20 +1,25 @@
-
+#!/usr/bin/env bash
 # MC data
 #db=/mnt/lfs6/sim/IceCube/2012/filtered/level2/neutrino-generator/11069/09000-09999/
 #gcd=/mnt/lfs6/sim/IceCube/2012/filtered/level2/neutrino-generator/11069/08000-08999/GeoCalibDetectorStatus_2012.56063_V1.i3.gz
 
-# MC data; local
-db=/scratch/mholm/database/I3files/
-gcd=/scratch/mholm/database/I3files/GeoCalibDetectorStatus_2012.56063_V1.i3.gz
+# database; local
+db=/groups/icecube/petersen/GraphNetDatabaseRepository/moon_pointing_analysis/monte_carlo/11069/I3files
 
-pulse="InIceDSTPulses"
+# pulse type
+pulse=InIceDSTPulses
 
-outdir=/scratch/mholm/database/
-report_location=/data/user/mholm/data/nohup_reports/MCL21.out
+report_name=MC
 
-nohup python /data/user/mholm/graphnet/studies/Moon_Pointing_Analysis/data/convert_i3_files.py \
+## do not alter beyond this point
+# date for report name
+TIMESTAMP=$(date "+%H%M%S")
+
+outdir=/groups/icecube/petersen/GraphNetDatabaseRepository/moon_pointing_analysis/monte_carlo/11069
+report_location=/groups/icecube/${USER}/storage/nohup_reports/${report_name}${TIMESTAMP}.out
+
+nohup python /groups/icecube/${USER}/work/analyses/moon_pointing_analysis/data/convert_i3_files.py \
 --db ${db} \
---gcd ${gcd} \
 --pulse ${pulse} \
 --outdir ${outdir} \
 > ${report_location}
