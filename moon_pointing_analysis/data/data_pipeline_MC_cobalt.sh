4965b8cee34e08fa75507edfbc5c2f646751fe55
Morten Holm <qgf305@hep02.cluster>
2022-10-17 22:07:32 +0200

    reformatted bash execution and templating

---
+++ moon_pointing_analysis/data/data_pipeline_MC_cobalt.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# Notes: points to the entire 1.1TB dataset of monte carlo numu data.
+
+# to recreate the results, follow the steps below.
+# (1) designate directory containing I3-files, including gcd file.
+database_directory=/mnt/lfs6/sim/IceCube/2012/filtered/level2/neutrino-generator/11069/09000-09999/
+#gcd=/mnt/lfs6/sim/IceCube/2012/filtered/level2/neutrino-generator/11069/08000-08999/GeoCalibDetectorStatus_2012.56063_V1.i3.gz
+# (2) designate output directory for created script.
+output_directory=/data/user/${USER}/storage/moon_pointing_analysis/monte_carlo/11069
+# (3) report output location and name
+report_directory=/data/user/${USER}/storage/nohup_reports/
+report_name=MC
+# (4) designate the feature keys to extract; found via investigating the I3-files using dataio-shovel in IceTray.
+keys=(TWSRTHVInIcePulses SplitInIcePulses InIceDSTPulses SplineMPE SplineMPEIC RNNReco RNNReco_sigma )
+
+
+## do not alter beyond this point ##
+# date for report name
+TIMESTAMP=$(date "+%H%M%S")
+
+# if directories does not exist for reporting, create them.
+mkdir -p ${output_directory};
+mkdir -p ${report_directory};
+
+# save the report file to 
+report_location=${report_directory}${report_name}${TIMESTAMP}.out
+
+# run script
+nohup python convert_i3_files.py \
+--db ${database_directory} \
+--outdir ${output_directory} \
+--keys ${keys} \
+> ${report_location}
---
+++ moon_pointing_analysis/data/data_pipeline_template.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# Notes: <this is a template, add relevant description here>
+
+# to recreate the results, follow the steps below by designating;
+# (1) directory containing I3-files, including gcd file.
+database_directory=/groups/icecube/petersen/GraphNetDatabaseRepository/...
+# (2) output directory for created script.
+output_directory=/groups/icecube/petersen/GraphNetDatabaseRepository...
+# (3) report output location and name
+report_directory=/groups/icecube/${USER}/storage/nohup_reports/
+report_name=<name of report>
+# (4) designate the feature keys to extract; found via investigating the I3-files using dataio-shovel in IceTray.
+keys=(key1 key2 ... keyn)
+
+
+## do not alter beyond this point ##
+# date for report name
+TIMESTAMP=$(date "+%H%M%S")
+
+# if directories does not exist for reporting, create them.
+mkdir -p ${output_directory};
+mkdir -p ${report_directory};
+
+# pending testing from the above dir creation; the above code should not override... hopefully
+#if [ ! -d dir ] 
+#then
+#    mkdir -p dir;
+#fi
+
+# save the report file to 
+report_location=${report_directory}${report_name}${TIMESTAMP}.out
+
+# run script
+nohup python convert_i3_files.py \
+--db ${database_directory} \
+--outdir ${output_directory} \
+--keys ${keys} \
+> ${report_location}
