5c79975a4ce3343a8e4f6f83e00503afe6ad3794
Morten Holm <qgf305@hep02.cluster>
2022-10-21 08:12:12 +0200

    correct comment wording

--- moon_pointing_analysis/plotting/event_location/plot_all_in_folder.sh
+++ moon_pointing_analysis/plotting/event_location/plot_all_in_folder.sh
@@ -1,6 +1,6 @@
 # to recreate the results, follow the steps below.
 bash_directory=$(dirname -- "$(readlink -f "${BASH_SOURCE}")")
-# (1) designate directory containing I3-files, including gcd file.
+# (1) designate directory containing database.
 database_directory=/groups/icecube/petersen/GraphNetDatabaseRepository/moon_pointing_analysis/real_data/data_with_reco/moonL4_segspline_exp13_01_merged_with_time_and_reco_and_new_pulsemap.db
 # (2) designate output directory for created script.
 output_directory=${bash_directory}/test_plot/
