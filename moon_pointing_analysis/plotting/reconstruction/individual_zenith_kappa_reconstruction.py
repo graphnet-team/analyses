dc1e2199e4da5a2030ebbad9137d988fc25bfab8
Morten Holm <qgf305@hep02.cluster>
2022-10-26 23:18:04 +0200

    radians to degrees and labeling

--- moon_pointing_analysis/plotting/reconstruction/individual_zenith_kappa_reconstruction.py
+++ moon_pointing_analysis/plotting/reconstruction/individual_zenith_kappa_reconstruction.py
@@ -3,6 +3,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
 import pandas as pd
 import numpy as np
 import argparse
+import math
 from helper_functions.plot_params import *
 
 parser = argparse.ArgumentParser(
@@ -42,6 +43,9 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
+def rad_to_deg(df):
+    return 180*df/math.pi
+
 # pattern to look for
 patterns = ["zenith", "azimuth"]
 # name of the database
@@ -55,22 +59,30 @@ for pattern, db_name in zip(patterns, db_names):
             locals()[db_name] = pd.read_csv(pathing)
 
 
-    plt.figure()
-    # make histogram of the pattern in the given db_name
-    mappable = plt.hist2d(
-        eval(db_name)[pattern+"_pred"],
-        eval(db_name)[pattern],
-        bins=args.bins,
-        cmap="viridis",
-    )
-    plt.title(f"results: {pattern} prediction")
-    colorbar(mappable[3])
-    plt.legend()
-    plt.savefig(args.output + pattern + "_results.png")
+    try:
+        plt.figure()
+        # make histogram of the pattern in the given db_name
+        mappable = plt.hist2d(
+            rad_to_deg(eval(db_name)[pattern+"_pred"]),
+            rad_to_deg(eval(db_name)[pattern]),
+            bins=args.bins,
+            cmap="viridis",
+        )
+        plt.title(f"results: {pattern} prediction")
+        colorbar(mappable[3])
+        plt.xlabel(pattern+"_pred")
+        plt.ylabel(pattern)
+        plt.legend()
+        plt.savefig(args.output + pattern + "_results.png")
+    except:
+        print(pattern + " failed")
 
-    plt.figure()
-    plt.hist(1 / np.sqrt(eval(db_name)["kappa"]), bins=args.bins)
-    plt.title("results: kappa prediction")
-    plt.yscale('log')
-    plt.legend()
-    plt.savefig(args.output + pattern + "_kappa_results.png")
+    try:
+        plt.figure()
+        plt.hist(1 / np.sqrt(eval(db_name)[pattern+"_kappa_pred"]), bins=args.bins)
+        plt.title("results: kappa prediction")
+        plt.yscale('log')
+        plt.legend()
+        plt.savefig(args.output + pattern + "_kappa_results.png")
+    except:
+        print(pattern + " failed for kappa")
