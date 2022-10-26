5a013bedfcf3cba20dca610f3cc67134a8d47695
Morten Holm <qgf305@hep02.cluster>
2022-10-26 23:22:13 +0200

    delete unused

--- moon_pointing_analysis/modelling/train.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-db="/groups/icecube/asogaard/data/sqlite/dev_lvl7_robustness_muon_neutrino_0000/data/dev_lvl7_robustness_muon_neutrino_0000.db"
-out="/groups/icecube/peter/workspace/graphnetmoon/graphnet/studies/Moon_Pointing_Analysis/modelling/TrainedModels/TestData/"
-#gpu=1
-
-python train_model_Azimuthal.py --database ${db} --output ${out} #--gpu ${gpu}
